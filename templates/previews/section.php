0e489c60189127e054948f07c2064566c4505284
Emran Ahmed <emran.bd.08@gmail.com>
2016-12-29 02:22:40 +0600

    Remove Attribute

--- templates/previews/section.php
+++ templates/previews/section.php
@@ -2,8 +2,6 @@
 
 <div v-preview-element :style="{'--background-color':model.attributes['background-color'], '--background-image':`url(${model.attributes.bgimage})`, '--background-image-position':model.attributes.bgimageposition}">
     <upb-preview-mini-toolbar :model="model"></upb-preview-mini-toolbar>
-
-    {{ model.attributes }}
-
+    
     <component v-for="(content, index) in model.contents" :index="index" :model="content" :is="content._upb_options.preview.component"></component>
 </div>
